a6b3efc71baa38d9a1cf66b954f8a8d9c1989181
priyankub <priyanku.mgr@gmail.com>
2024-02-23 10:14:08 -0500

    change the display output

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httputil"
+	"strings"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -40,12 +41,32 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Display request and response details in the browser
-	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintln(w, "Request:")
+	// Extract IP and headers from Via header
+	viaHeader := resp.Header.Get("Via")
+	viaEntries := strings.Split(viaHeader, ", ")
+	ipAndHeaders := make([][]string, len(viaEntries))
+	for i, entry := range viaEntries {
+		ipAndHeaders[i] = strings.SplitN(entry, " ", 2)
+	}
+
+	// Display request and response details along with IP and headers in a table
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintln(w, "<html><head><title>Proxy Hops</title></head><body>")
+	fmt.Fprintln(w, "<h2>Proxy Hops:</h2>")
+	fmt.Fprintln(w, "<table border=\"1\"><tr><th>IP</th><th>Headers Added</th></tr>")
+	for _, entry := range ipAndHeaders {
+		fmt.Fprintf(w, "<tr><td>%s</td><td>%s</td></tr>\n", entry[0], entry[1])
+	}
+	fmt.Fprintln(w, "</table>")
+	fmt.Fprintln(w, "<h2>Request:</h2>")
+	fmt.Fprintln(w, "<pre>")
 	fmt.Fprintln(w, string(requestDump))
-	fmt.Fprintln(w, "\nResponse:")
+	fmt.Fprintln(w, "</pre>")
+	fmt.Fprintln(w, "<h2>Response:</h2>")
+	fmt.Fprintln(w, "<pre>")
 	fmt.Fprintln(w, string(responseDump))
+	fmt.Fprintln(w, "</pre>")
+	fmt.Fprintln(w, "</body></html>")
 }
 
 func main() {
